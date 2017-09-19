947111c2b411147db516eed14aa5e68cc09f52d7
jzon017 <djsablay@gmail.com>
2017-09-19 11:45:27 +0800

    FaceTracker
    testupload

---
+++ FaceTracker/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/PubNubClass.java
@@ -0,0 +1,100 @@
+package com.google.android.gms.samples.vision.face.facetracker;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.enums.PNStatusCategory;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+
+/**
+ * Created by Thomas Joel E. Tan on 9/17/2017.
+ */
+
+public class PubNubClass {
+    private static String PubKey = "pub-c-79b5e373-b1f1-48df-86ad-f9c48ad9355c" ;
+    private static String SubKey = "sub-c-e642b25c-92ec-11e7-96ff-be2ded2a9039";
+
+    public static PubNub PubCon(PubNub pubnub, PNConfiguration pnConfiguration)
+    {
+        pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey(SubKey);
+        pnConfiguration.setPublishKey(PubKey);
+        pnConfiguration.setSecure(false);
+        return pubnub = new PubNub(pnConfiguration);
+    }
+
+    public static void PubNubPublisher(PubNub pubnub, final float level)
+    {
+        pubnub.addListener(new SubscribeCallback() {
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+
+
+                if (status.getCategory() == PNStatusCategory.PNUnexpectedDisconnectCategory) {
+                    // This event happens when radio / connectivity is lost
+                } else if (status.getCategory() == PNStatusCategory.PNConnectedCategory) {
+
+                    // Connect event. You can do stuff like publish, and know you'll get it.
+                    // Or just use the connected event to confirm you are subscribed for
+                    // UI / internal notifications, etc
+
+                    if (status.getCategory() == PNStatusCategory.PNConnectedCategory) {
+                        pubnub.publish().channel("Channel-c05riknj4").message(level).async(new PNCallback<PNPublishResult>() {
+                            @Override
+                            public void onResponse(PNPublishResult result, PNStatus status) {
+                                // Check whether request successfully completed or not.
+                                if (!status.isError()) {
+
+                                    // Message successfully published to specified channel.
+                                }
+                                // Request processing failed.
+                                else {
+
+                                    // Handle message publish error. Check 'category' property to find out possible issue
+                                    // because of which request did fail.
+                                    //
+                                    // Request can be resent using: [status retry];
+                                }
+                            }
+                        });
+                    }
+                } else if (status.getCategory() == PNStatusCategory.PNReconnectedCategory) {
+
+                    // Happens as part of our regular operation. This event happens when
+                    // radio / connectivity is lost, then regained.
+                } else if (status.getCategory() == PNStatusCategory.PNDecryptionErrorCategory) {
+
+                    // Handle messsage decryption error. Probably client configured to
+                    // encrypt messages and on live data feed it received plain text.
+                }
+            }
+
+            @Override
+            public void message(PubNub pubnub, PNMessageResult message) {
+                // Handle new message stored in message.message
+                if (message.getChannel() != null) {
+                    // Message has been received on channel group stored in
+                    // message.getChannel()
+                } else {
+                    // Message has been received on channel stored in
+                    // message.getSubscription()
+                }
+
+            /*
+                log the following items with your favorite logger
+                    - message.getMessage()
+                    - message.getSubscription()
+                    - message.getTimetoken()
+            */
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+
+            }
+        });
+    }
+}
